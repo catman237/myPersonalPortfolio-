3175254b14f1c6fe93c0da8fa1100ffd992a184f
Greg <Ghughes610@gmail.com>
2021-04-19 22:38:10 -0600

    added simple js and started about me

---
+++ script.js
@@ -0,0 +1,8 @@
+const img = document.getElementById("kelsoClimbing")
+
+function clickAlert() {
+    alert('This is my wife!')
+}
+
+img.addEventListener('click', clickAlert);
+img.addEventListener ('hover')
